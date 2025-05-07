85ae25e8972f00b0e17de105a8832c44b36e77c6
Kent224 <ken.w.0920@gmail.com>
2025-05-08 00:21:56 +0900

    静的サイト化や不要ファイル整理など現状反映

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
-export default {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
-}; 
+};
+
+export default nextConfig; 
